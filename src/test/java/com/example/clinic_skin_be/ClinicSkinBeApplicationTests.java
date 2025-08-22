b75f46ddb9606ebde88c818d09371f82e9123270
NgocThom <thomd1209@gmail.com>
2025-08-22 09:47:36 +0700

    init

--- src/test/java/com/example/clinic_skin_be/ClinicSkinBeApplicationTests.java
+++ src/test/java/com/example/clinic_skin_be/ClinicSkinBeApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HospitalCareBeApplicationTests {
+class ClinicSkinBeApplicationTests {
 
     @Test
     void contextLoads() {
