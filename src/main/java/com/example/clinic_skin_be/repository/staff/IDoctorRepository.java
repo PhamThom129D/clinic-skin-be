f55db6c554cb939a759e4802ed472a4a3dea31b1
Thuy Pham <phamthikimthuy05@gmail.com>
2025-08-27 02:57:26 +0700

    do data offer xong

--- src/main/java/com/example/clinic_skin_be/repository/staff/IDoctorRepository.java
+++ src/main/java/com/example/clinic_skin_be/repository/staff/IDoctorRepository.java
@@ -2,6 +2,8 @@ package com.example.clinic_skin_be.repository.staff;
 
 import com.example.clinic_skin_be.model.staff.doctor.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IDoctorRepository extends JpaRepository<Doctor, Long> {
 }
