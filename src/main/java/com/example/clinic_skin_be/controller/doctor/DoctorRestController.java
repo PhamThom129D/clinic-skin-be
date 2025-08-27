de4ec3582cc6726ee840b34e48df0d23fc3bd257
Thuy Pham <phamthikimthuy05@gmail.com>
2025-08-27 15:45:35 +0700

    saved do data

--- src/main/java/com/example/clinic_skin_be/controller/doctor/DoctorRestController.java
+++ src/main/java/com/example/clinic_skin_be/controller/doctor/DoctorRestController.java
@@ -1,8 +1,7 @@
 package com.example.clinic_skin_be.controller.doctor;
 
-import com.example.clinic_skin_be.dto.staff.DoctorInfoDTO;
-import com.example.clinic_skin_be.model.staff.doctor.Doctor;
 import com.example.clinic_skin_be.repository.staff.IDoctorRepository;
+import com.example.clinic_skin_be.service.staff.DoctorService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,24 +9,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 @RestController
 @RequestMapping ("/api/doctors")
 @AllArgsConstructor
 public class DoctorRestController {
-    @Autowired
-    private IDoctorRepository doctorRepository;
+    private final DoctorService doctorService;
 
     @GetMapping("/basic")
-    public List<DoctorInfoDTO> getAllDoctors () {
-        List<Doctor> doctors = doctorRepository.findAll();
-        return doctors.stream().map(doctor -> new DoctorInfoDTO(
-               doctor.getId(),
-                doctor.getAccount().getFullName(),
-                doctor.getSpecialty(),
-                doctor.getAccount().getAvtPath()
-        )).collect(Collectors.toList());
+    public List<Map<String, Object>> getAllDoctors () {
+        return doctorService.getAllDoctorsInfo();
     }
 }
--- src/main/java/com/example/clinic_skin_be/controller/screen/TestimonialRestController.java
+++ src/main/java/com/example/clinic_skin_be/controller/screen/TestimonialRestController.java
@@ -1,7 +1,5 @@
 package com.example.clinic_skin_be.controller.screen;
 
-import com.example.clinic_skin_be.dto.screen.TestimonialInfoDTO;
-import com.example.clinic_skin_be.model.screen.Testimonial;
 import com.example.clinic_skin_be.repository.screen.ITestimonialRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/testimonials")
@@ -20,14 +18,7 @@ public class TestimonialRestController {
     private ITestimonialRepository testimonialRepository;
 
     @GetMapping("")
-    public List<TestimonialInfoDTO> getAllTestimonials () {
-        List<Testimonial> testimonials = testimonialRepository.findAll();
-        return testimonials.stream().map(testimonial -> new TestimonialInfoDTO(
-                testimonial.getId(),
-                testimonial.getContent(),
-                testimonial.getImg(),
-                testimonial.getAccount().getId(),
-                testimonial.getAccount().getFullName()
-        )).collect(Collectors.toList());
+    public List<Map<String, Object>> getAllTestimonials () {
+        return testimonialRepository.findAllTestimonials();
     }
 }
