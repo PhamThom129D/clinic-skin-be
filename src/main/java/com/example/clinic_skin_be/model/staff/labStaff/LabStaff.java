ee0482061aa2dcfa81c9ed3124d6b71fa279cc52
NgocThom <thomd1209@gmail.com>
2025-09-18 10:34:59 +0700

    manage staff

--- src/main/java/com/example/clinic_skin_be/model/staff/labStaff/LabStaff.java
+++ src/main/java/com/example/clinic_skin_be/model/staff/labStaff/LabStaff.java
@@ -2,9 +2,15 @@ package com.example.clinic_skin_be.model.staff.labStaff;
 
 import com.example.clinic_skin_be.model.user.Account;
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "lab_staff")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class LabStaff {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
